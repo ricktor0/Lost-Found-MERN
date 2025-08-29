a7484da474a697babb418a9cc584a8cac7d4e4be
Vikram Sharma <135196573+ricktor0@users.noreply.github.com>
2025-08-29 13:12:13 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ dotenv.config();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://lost-found-frontend.onrender.com/",
     credentials: true,
   })
 );
